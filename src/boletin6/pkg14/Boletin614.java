e5004f5882c10899460566ebc60c9e982d6fcc3b
aferreiradominguez <aferreiradominguez@eniac27.danielcastelao.org>
2014-10-22 11:13:45 +0200

    Pazó rules

---
+++ src/boletin6/pkg14/Boletin614.java
@@ -0,0 +1,15 @@
+
+package boletin6.pkg14;
+
+
+public class Boletin614 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        
+    }
+    
+}
---
+++ src/boletin6/pkg14/Tenda.java
@@ -0,0 +1,15 @@
+/*(código: C1, precio: 24 euros), (código: H23, precio: 234 euros) y (código: M30, 
+precio: 109 euros). A continuación debe cambiarse o segundo producto da factura 
+polo seguinte (código: K123, precio:247 euros) e volve  a imprimir a factura */
+package boletin6.pkg14;
+
+public class Tenda {
+private String codigo;
+private float precio;
+
+public Tenda (String c , float p){
+    codigo=c;
+    precio=p;
+}
+String codigoC1
+}
