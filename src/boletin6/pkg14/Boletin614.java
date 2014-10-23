e73b579d4615cce1e960717365c43b7f4d154f55
aferreiradominguez <aferreiradominguez@eniac27.danielcastelao.org>
2014-10-23 11:11:29 +0200

    FEITO :)

--- src/boletin6/pkg14/Boletin614.java
+++ src/boletin6/pkg14/Boletin614.java
@@ -1,4 +1,6 @@
-
+/*(código: C1, precio: 24 euros), (código: H23, precio: 234 euros) y (código: M30, 
+precio: 109 euros). A continuación debe cambiarse o segundo producto da factura 
+polo seguinte (código: K123, precio:247 euros) e volve  a imprimir a factura */
 package boletin6.pkg14;
 
 
@@ -8,6 +10,25 @@ public class Boletin614 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+     Tenda tenda1=new Tenda("C1",24f);
+     Tenda tenda2=new Tenda("H23",234f);
+     Tenda tenda3=new Tenda("M30",109f);
+        System.out.println("prezos do C1, H23 e M30"+"\n"+tenda1.getCodigo()+"\t"+tenda1.getPrezo()+"€"+"\n"+tenda2.getCodigo()+"\t"
+        +tenda2.getPrezo()+"€"+"\n"+tenda3.getCodigo()+"\t"+tenda3.getPrezo()+"€");
+     Tenda tenda4=new Tenda("K123",247f);
+     System.out.println("prezos do C1, H23 e M30"+"\n"+tenda1.getCodigo()+"\t"+tenda1.getPrezo()+"€"+"\n"+tenda4.getCodigo()+"\t"
+        +tenda4.getPrezo()+"€"+"\n"+tenda3.getCodigo()+"\t"+tenda3.getPrezo()+"€");
+        
+    
+  
+        
+     
+     
+     
+     
+     
+     
+     
      
         
     }
--- src/boletin6/pkg14/Tenda.java
+++ src/boletin6/pkg14/Tenda.java
@@ -1,15 +1,31 @@
-/*(código: C1, precio: 24 euros), (código: H23, precio: 234 euros) y (código: M30, 
-precio: 109 euros). A continuación debe cambiarse o segundo producto da factura 
-polo seguinte (código: K123, precio:247 euros) e volve  a imprimir a factura */
+
 package boletin6.pkg14;
 
 public class Tenda {
 private String codigo;
-private float precio;
+private float prezo;
+
+
 
 public Tenda (String c , float p){
-    codigo=c;
-    precio=p;
+    codigo= c;
+    prezo=p;
+}
+
+    Tenda() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+public void setCodigo(String codigo){
+    this.codigo=codigo;
+    }
+public void setPrezo(float prezo){
+    this.prezo=prezo;
+    }
+public String getCodigo(){
+    return codigo;
+}
+public float getPrezo(){
+    return prezo;
 }
 
 }
